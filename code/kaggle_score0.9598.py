e7fda23e649c532932d9fddfc0efb8b981cc82f7
fywfyw111 <45417421+fywfyw111@users.noreply.github.com>
2020-03-01 20:26:02 +0800

    Update kaggle_score0.9598.py

--- code/kaggle_score0.9598.py
+++ code/kaggle_score0.9598.py
@@ -2,8 +2,8 @@
 # It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
 # For example, here's several helpful packages to load in
 
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np  # linear algebra  计算
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)数据预处理
 
 # Input data files are available in the "../input/" directory.
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
